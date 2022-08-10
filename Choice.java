9c20551ae614e824443c0a31597c30f2e186a3fe
marissashey <ms10791@nyu.edu>
2022-08-09 22:46:01 -0400

    Create Choice.java

---
+++ Choice.java
@@ -0,0 +1,19 @@
+public class Choice {
+    public int choice;
+
+    public Choice(int choice){
+        this.choice = choice;
+    }
+
+    public int getChoice(){
+        return this.choice;
+    }
+
+    public void setChoice(int choice){
+        this.choice = choice;
+    }
+
+    public void print(){
+        System.out.println("Placeholder");
+    }
+}
